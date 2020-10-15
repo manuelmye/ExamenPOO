e6545de5d7775cde7cafe95ea49eae4e14b0a835
manuelmye <65754885+manuelmye@users.noreply.github.com>
2020-10-15 09:30:01 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,7 @@ import data.itemsData;
 import items.items;
 import java.util.Scanner;
 
-
 public class Main {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ItemsData itemsData = new ItemsData();
